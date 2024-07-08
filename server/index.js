2f4ed8643c597b4159a6792cd77d0addd8750358
mridul589 <tmridul79@gmail.com>
2024-07-08 19:56:19 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -30,18 +30,7 @@ app.use(express.json());
 app.use("/api/user",userRoutes);
 app.use("/api/admin",adminRoutes);
 
-const NODE_ENV = "production";
-const __currdir = path.resolve();
-if (NODE_ENV === "production") {
-  app.use(express.static(path.join(__currdir, "/client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__currdir, "client", "dist", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    return res.send("API is running!");
-  });
-}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
